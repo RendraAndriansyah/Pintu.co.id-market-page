aaef4384a731d601d88aabd7ebb2758f80df29b6
travelite <rendrabootcamp@gmail.com>
2023-03-16 05:06:03 +0700

    responsive top movers

--- src/components/TopMovers.jsx
+++ src/components/TopMovers.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 const TopMovers = ({ price, movers, name, logo, keys }) => {
   return (
-    <div className=" pt-4  hover:bg-slate-200 rounded-lg" key={keys}>
+    <div className=" pt-4  hover:bg-slate-200 rounded-lg  w-32 lg:w-auto " key={keys}>
       <div className="p-2 ">
-        <div className="flex items-center">
-          <img src={logo} alt="ptu-token" className="inline w-12" />
-          <p className="text-xl font-bold text-center pl-2">{name}</p>
+        <div className="flex items-center ">
+          <img src={logo} alt="ptu-token" className="flex-none w-10 lg:w-10" />
+          <p className="text-sm lg:text-lg font-bold text-center pl-2 text-inherit">
+            {name}
+          </p>
         </div>
         <p className="text-zinc-400 py-2">{price}</p>
         <div
           className={`${
             movers >= 0 ? "text-green-500" : "text-red-500"
-          } text-2xl flex align-middle`}
+          } text-xl lg:text-2xl flex align-middle`}
         >
           <span className="text-lg pr-1">▲</span>
           <span className="font-medium">{`${movers}%`}</span>
